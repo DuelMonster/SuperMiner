df948d290653162b8a238782a7b1ba7238df926f
DuelMonster <GitHub@DuelMonster.co.uk>
2017-01-03 22:53:14 +0000

    Config Localization Fix

--- src/main/java/duelmonster/superminer/objects/Globals.java
+++ src/main/java/duelmonster/superminer/objects/Globals.java
@@ -21,7 +21,6 @@ import net.minecraft.block.BlockRotatedPillar;
 import net.minecraft.block.BlockStone;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.item.EntityXPOrb;
@@ -39,10 +38,12 @@ import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.registry.RegistryNamespaced;
 import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.translation.I18n;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.client.FMLClientHandler;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 
+@SuppressWarnings("deprecation")
 public class Globals {
 	public static final String ChannelName = "SuperMiner";
 	
@@ -319,8 +320,8 @@ public class Globals {
 	
 	public static String localize(String key) {
 		if (FMLCommonHandler.instance().getSide().isClient())
-			if (I18n.hasKey(key))
-				return I18n.format(key);
+			if (I18n.canTranslate(key))
+				return I18n.translateToLocal(key);
 			
 		return key;
 	}
