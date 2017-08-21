8a56adcb2efa3c5b9e558a7b9233f975954ad6ca
DuelMonster <GitHub@DuelMonster.co.uk>
2017-08-21 21:18:00 +0100

    Updated to MC 1.12.1

--- src/main/java/duelmonster/superminer/config/SMGuiFactory.java
+++ src/main/java/duelmonster/superminer/config/SMGuiFactory.java
@@ -11,29 +11,17 @@ public class SMGuiFactory implements IModGuiFactory {
 	public void initialize(Minecraft minecraftInstance) {}
 	
 	@Override
-	public Class<? extends GuiScreen> mainConfigGuiClass() {
-		return SMGuiConfig.class;
-	}
-	
-	@Override
 	public Set<RuntimeOptionCategoryElement> runtimeGuiCategories() {
 		return null;
 	}
-
+	
 	@Override
 	public boolean hasConfigGui() {
 		return true;
 	}
-
+	
 	@Override
 	public GuiScreen createConfigGui(GuiScreen parentScreen) {
 		return new SMGuiConfig(parentScreen);
-	}
-
-	@SuppressWarnings("deprecation")
-	@Override
-	public RuntimeOptionGuiHandler getHandlerFor(RuntimeOptionCategoryElement element) {
-		// TODO Auto-generated method stub
-		return null;
 	}
 }
